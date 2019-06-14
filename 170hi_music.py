0a03c346b31d17135093848b768291a25bc3d87f
root <root@DESKTOP-4JVPIQ0.localdomain>
2019-06-14 17:57:31 +0800

    使用条件

--- 170hi_music.py
+++ 170hi_music.py
@@ -6,17 +6,12 @@ from pypinyin import lazy_pinyin
 import eyed3
 
 '''
-先从搜索链接获取歌曲信息(json)
-abslist 返回的结果字段,先判断这key是否有值
-MUSICRID 曲ID  MUSIC_157607;
-SONGNAME 曲名 慢慢;
-ARTIST 歌手 张学友;
-FALBUM 专辑 Black&nbsp;&&nbsp;White;
-WEBMP3Q0_PATH 歌曲的下载后缀 /n2/128/66/52/3763799718.mp3
-http://other.web.ra01.sycdn.kuwo.cn/resource/n2/128/66/52/3763799718.mp3
-酷我音乐api "http://www.kuwo.cn/webmusic/st/getMuiseByRid?rid=MUSIC_"
-&nbsp; 空格标签
-&amp; &标签
+1.从疯狂音乐网站搜索歌曲的下载信息;
+2.从酷我音乐库中搜索歌曲信息的补充信息(作者，专辑，流派)；
+3.下载mp3文件后修改文件缺失的一些属性（强迫症患者）
+4.支持香港反送中大游行者可下载使用
+&nbsp; 表示 空格
+&amp; 表示 &
 '''
 
 music_dir="E:\\音乐\\"
@@ -24,17 +19,7 @@ search_url ="http://search.kuwo.cn/r.s"
 referer_url = "http://www.170hi.com/so?q="
 kuwo_api="http://www.kuwo.cn/webmusic/st/getMuiseByRid?rid="
 download_url = "http://other.web.ra01.sycdn.kuwo.cn/resource/"
-data = {
-    "all": u"练习",
-    "ft":"music",
-    "client":"kt",
-    "cluster":"0",
-    "rn":"1",
-    "rformat":"json",
-    "callback":"searchMusicResult",
-    "encoding":"utf8",
-    "vipver":"MUSIC_8.0.3.1"
-}
+
 headers={
     "Content-Type":"application/json",
     "Cache-Control":"max-age=0",
@@ -50,9 +35,8 @@ download_header={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
 }
 
-def get_music_download_url(name,artist=None,rn='5'):
+def get_music_download_url(name,artist=None,rn='7'):
     china_name = name
-    pinyin_name = "".join(lazy_pinyin(name))
     name = name.encode('utf-8')
     headers["Referer"] = referer_url+str(name)+"&nsid=4"
     data["all"] = name
@@ -64,41 +48,25 @@ def get_music_download_url(name,artist=None,rn='5'):
     str_result= re_result.group().strip("=")
     json_result = eval(str_result)
     # print(json_result["abslist"][0])
-    music_model = {"ARTIST":artist,"NAME":china_name,"FALBUM":"","ID":"","URL":"","TITLE":"","PNAME":pinyin_name,"MPATH":music_dir}
+    music_model = {"ARTIST":artist,"NAME":china_name,"MPATH":music_dir}
     if json_result["abslist"]:
         music_model["ARTIST"] = json_result["abslist"][0]["ARTIST"]
         music_model["FALBUM"] = json_result["abslist"][0]["ALBUM"]
         music_model["GENRE"] = json_result["abslist"][0]["GENRE"]
-        music_model["URL"] = json_result["abslist"][0]["MP3PATH"]
-        music_model["URL2"] = json_result["abslist"][0]["WEBMP3Q0_PATH"]
         music_model["ID"] = json_result["abslist"][0]["MUSICRID"]
         music_model["TITLE"] = str(china_name)
-        music_model["SAVE_PATH"] = music_dir + json_result["abslist"][0]["ARTIST"]
         if artist:
             for i in json_result["abslist"]:
                 if i["ARTIST"] == artist.replace(" ","&nbsp;"):
                     music_model["ARTIST"]= str(artist)
                     music_model["FALBUM"]=i["ALBUM"]
                     music_model["GENRE"]=i["GENRE"]
-                    music_model["URL"]=i["MP3PATH"]
-                    music_model["URL2"]=i["WEBMP3Q0_PATH"]
                     music_model["ID"]=i["MUSICRID"]
                     music_model["TITLE"]=str(china_name)+"-"+str(artist)
-                    music_model["SAVE_PATH"]=music_dir+str(artist)
         print("酷我音乐 歌手：%s 歌曲：%s 专辑：%s 流派：%s"% (music_model["ARTIST"],music_model["NAME"],music_model["FALBUM"],music_model["GENRE"]))
         return music_model
     else:
         return None
-
-def get_kuwo_api_callback(music_dict):
-    music_id=music_dict["ID"]
-    kuwo_url=kuwo_api+music_id
-    res=requests.get(kuwo_url)
-    name = re.search(r'mp3dl>(.*)</mp3dl',res.text)
-    name = "http://"+name.group(1)+"/resource/"
-    music_dict["URL"]=name+music_dict["URL"]
-    music_dict["URL2"]=name+music_dict["URL2"]
-    return music_dict
 
 
 def mk_music_dir(path):
@@ -108,46 +76,8 @@ def mk_music_dir(path):
     if not isExists:
         os.makedirs(path)
 
-def download_music(music_dict,dir=None):
-    if not dir:
-        dir = music_dict["SAVE_PATH"]
-    mk_music_dir(dir)
-    print("下载地址1: %s"% music_dict["URL"])
-    print("下载地址2: %s"% music_dict["URL2"])
-    try:
-        wget = requests.get(music_dict["URL"])
-        if wget.status_code == 404:
-            print("地址1下载失败,开始地址2下载尝试")
-            wget = requests.get(music_dict["URL2"])
-    except:
-        print("地址1下载失败,开始地址2下载尝试")
-        wget = requests.get(music_dict["URL2"])
-
-    file_name = '%s%s.mp3'% (music_dict["MPATH"],music_dict["PNAME"])
-    with open(file_name,'wb') as f:
-        f.write(wget.content)
-        print("已下载 %s"% music_dict["NAME"])
-    return file_name
-
-def charactor_mp3(filename,music_dict):
-    '''&nbsp; 空格标签 &amp; &标签'''
-    f = eyed3.load(filename)
-    f.initTag()
-    artist=music_dict["ARTIST"].replace("&nbsp;"," ")
-    album=music_dict["FALBUM"].replace("&nbsp;"," ")
-    title=music_dict["NAME"]
-    album=album.replace("&amp;","&")
-    print(artist,album,title)
-    f.tag.artist=artist
-    f.tag.album_artist = artist
-    f.tag.album=album
-    f.tag.title =title
-    f.tag.save()
-    music_file_name=music_dict["SAVE_PATH"]+"\\"+'%s.mp3'% music_dict["NAME"]
-    os.rename(filename,music_file_name)
-
 def search_music_from_ifkdy(name,artist=None):
-    """疯狂音乐搜索,基于咪咕音乐库"""
+    """疯狂音乐搜索,基于QQ音乐库"""
     kuwo_data = get_music_download_url(name,artist)
     headers = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -215,8 +145,4 @@ def search_music_from_ifkdy(name,artist=None):
             print("此文件已存在")
 
 if __name__ == "__main__":
-    # get_value=get_music_download_url(u"走马",artist=u"陈粒",rn='20')
-    # Dict=get_kuwo_api_callback(get_value)
-    # filename=download_music(Dict)
-    # charactor_mp3(filename,Dict)
-    search_music_from_ifkdy(u"I'm Gonna Getcha Good",artist=u"Shania Twain")
+    search_music_from_ifkdy(u"海阔天空",artist=u"beyond")
